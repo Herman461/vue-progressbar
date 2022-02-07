2a7226025a5e0729f354fa53396039bf057bb97b
Herman461 <zhukovskiyherman@gmail.com>
2022-02-07 10:02:51 +0300

    added circle

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,133 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="page">
+    <div class="page__progress-bar">
+      <svg height="220" width="220">
+        <linearGradient id="linear">
+          <stop offset="95%" stop-color="#8932f5" />
+          <stop offset="5%" stop-color="#6304d9" />
+        </linearGradient>
+        <circle cx="110" cy="110" r="100" fill="transparent" stroke-width="strokeWidth" stroke="#8321fc"></circle>
+        <circle cx="110" cy="110" r="100" fill="transparent" stroke-width="strokeWidth" stroke="url(#linear)" stroke-dashoffset="100" stroke-dasharray="628"></circle>
+      </svg>
+    </div>
+    <form class="page__form page-form">
+      <div class="page-form__input">
+        <input-number step="1" @increase-value="changeStrokeWidth" />
+      </div>
+    </form>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import InputNumber from "./components/InputNumber";
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {InputNumber},
+  data() {
+    return {
+      strokeWidth: 6
+    }
+  }
+  methods: {
+
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  padding: 0;
+  margin: 0;
+  border: 0;
 }
+
+*,
+*:before,
+*:after {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+:focus,
+:active {
+  outline: none;
+}
+
+a:focus,
+a:active {
+  outline: none;
+}
+
+nav,
+footer,
+header,
+aside {
+  display: block;
+}
+
+html,
+body {
+  height: 100%;
+  width: 100%;
+  font-size: 100%;
+  line-height: 1;
+  font-size: 14px;
+  -ms-text-size-adjust: 100%;
+  -moz-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+}
+
+input,
+button,
+textarea {
+  font-family: inherit;
+}
+
+input::-ms-clear {
+  display: none;
+}
+
+button {
+  cursor: pointer;
+}
+
+button::-moz-focus-inner {
+  padding: 0;
+  border: 0;
+}
+
+a,
+a:visited {
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: none;
+}
+
+ul li {
+  list-style: none;
+}
+
+img {
+  vertical-align: top;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-size: inherit;
+  font-weight: 400;
+}
+
+* {
+  min-height: 0;
+  min-width: 0;
+}
+
 </style>
---
+++ src/components/InputNumber.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="input-number">
+    <div class="input-number__button input-number__button--plus">+</div>
+    <div class="input-number__item">
+      <input class="input" type="text" autocomplete="off" />
+    </div>
+    <div class="input-number__button input-number__button--minus">-</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "input-number",
+
+}
+</script>
+
+<style lang="scss">
+.input-number {
+  width: 120px;
+  height: 40px;
+  display: flex;
+  &__button {
+    flex: 0 0 40px;
+    background-color: #6304d9;
+    color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 22px;
+    font-weight: 700;
+  }
+  &__item {
+    flex: 1 1 auto;
+
+  }
+  .input {
+    display: block;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    padding: 1px 5px;
+  }
+}
+</style>
